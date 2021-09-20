9d6abc2a782ec1e1374cb3191e5d656a56ab5e56
Pedro <pedrobiqua@gmail.com>
2021-09-20 11:05:44 -0300

    first commit

---
+++ EnviarEmailParaGit/EnviarEmail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace EnviarEmailParaGit
+{
+    public class EnviaEmail
+    {
+        public static void EnviarEmail(String mailDestino)
+        {
+            try
+            {
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    using (MailMessage email = new MailMessage())
+                    {
+                        smtp.Host = "smtp.gmail.com";
+                        smtp.UseDefaultCredentials = false;
+                        smtp.Credentials = new System.Net.NetworkCredential("email smtp", "senha");
+                        smtp.Port = 587; // porta padrão
+                        smtp.EnableSsl = true; // essa opção tem q ser true pois o gmail tem um certificado ssl
+
+                        //Mensagem e remetente e destinatario
+                        email.From = new MailAddress("email");
+                        email.To.Add(mailDestino);
+
+                        email.Subject = "Estudando C#";
+                        email.IsBodyHtml = false;
+                        email.Body = "Estudo de c# enviando um e-mail com um codigo meu";
+
+                        //Enviar o email
+                        smtp.Send(email);
+                    }
+                }
+                Console.WriteLine("E-mail enviado");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ EnviarEmailParaGit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnviarEmailParaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque o e-mail para ser enviado");
+            var input = Console.ReadLine();
+            EnviaEmail.EnviarEmail(input);
+            Console.ReadLine();
+        }
+    }
+}
